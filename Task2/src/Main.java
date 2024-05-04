de68143edc2395036d495863e1c95d7cd68123c4
crin706 <rosh-ka@mail.ru>
2024-05-04 16:20:28 +0300

    Неплохой валидатор ФИО

--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //todo код писать тут. По желанию(рекомендуется) вынести код в методы.
+        checkIfContainRusLetters(getLine());
+    }
+
+    private static void checkIfContainRusLetters(String test) {
+        String CORRECT_CHARS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя-АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+        boolean isValid = true;
+        String myNewString = test.replaceAll(" ", "");
+        for (int i = 0; i < myNewString.length(); i++) {
+            if (!CORRECT_CHARS.contains(String.valueOf(myNewString.charAt(i)))) {
+                isValid = false;
+                break;
+            }
+        }
+        if (isValid) {
+            String[] arrayFromScanner = test.split(" ");
+            System.out.println("Фамилия: " + arrayFromScanner[0]);
+            System.out.println("Имя: " + arrayFromScanner[1]);
+            System.out.println("Отчество: " + arrayFromScanner[2]);
+        } else {
+            System.out.println("Введенная строка не является ФИО.");
+        }
+    }
+
+    private static String getLine() {
+        return new Scanner(System.in).nextLine();
     }
 }
